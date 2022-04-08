cda9b52b35ecf2f3a2b0112185d804c6543be516
MGonza20 <64711979+MGonza20@users.noreply.github.com>
2022-04-07 23:40:06 -0600

    Add files via upload

---
+++ src/components/Board.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from './Card'
+import './board.css'
+
+export default function Board({cardsProp, onFlip, card1, card2, block}) { 
+  
+  return (
+    <div className="board">
+      {cardsProp.map((element, index) => (
+          <Card 
+          key={index}
+          cardElement={element} 
+          onFlip={onFlip} 
+          block={block}
+          flipIt={element.paired || element === card1 || element === card2 }/>
+      ))}
+    </div>
+  )
+}
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './card.css'
+
+export default function Card({cardElement, onFlip, flipIt, block}) { 
+
+  return (
+    <div className={`card ${flipIt ? 'see' : ''}`}  
+         onClick={() => !block ? onFlip(cardElement.id, cardElement) : ''}>
+    <div 
+        className="icon"> <img src={cardElement.src} ></img>  </div>
+    </div> 
+  )
+}
