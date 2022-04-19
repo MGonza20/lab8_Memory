9cf7d68b182fa03627b8d5d76392a794d155cbaf
MGonza20 <pag20634@uvg.edu.gt>
2022-04-19 15:18:41 -0600

    formateando documento con prettier

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,18 +3,18 @@ import React from "react";
 import Card from "./Card";
 import "./board.css";
 
-export default function Board({cardsProp, onFlip, card1, card2, block}) { 
-  
-    return (
-        <div className="board">
-            {cardsProp.map((element, index) => (
-                <Card 
-                    key={index}
-                    cardElement={element} 
-                    onFlip={onFlip} 
-                    block={block}
-                    flipIt={element.paired || element === card1 || element === card2 }/>
-            ))}
-        </div>
-    );
+export default function Board({ cardsProp, onFlip, card1, card2, block }) {
+  return (
+    <div className="board">
+      {cardsProp.map((element, index) => (
+        <Card
+          key={index}
+          cardElement={element}
+          onFlip={onFlip}
+          block={block}
+          flipIt={element.paired || element === card1 || element === card2}
+        />
+      ))}
+    </div>
+  );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,16 @@
 import React from "react";
 import "./card.css";
 
-export default function Card({cardElement, onFlip, flipIt, block}) { 
-
-    return (
-        <div className={`card ${flipIt ? "see" : ""}`}  
-            onClick={() => !block ? onFlip(cardElement.id, cardElement) : ""}>
-            <div 
-                className="icon"> <img src={cardElement.src} ></img>  </div>
-        </div> 
-    );
+export default function Card({ cardElement, onFlip, flipIt, block }) {
+  return (
+    <div
+      className={`card ${flipIt ? "see" : ""}`}
+      onClick={() => (!block ? onFlip(cardElement.id, cardElement) : "")}
+    >
+      <div className="icon">
+        {" "}
+        <img src={cardElement.src}></img>{" "}
+      </div>
+    </div>
+  );
 }
