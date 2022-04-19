0e1dd13426649625f9660d24f95f4cc50fb12165
MGonza20 <pag20634@uvg.edu.gt>
2022-04-19 13:31:03 -0600

    Arreglando errores indicados por Eslint

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,14 @@
+/* eslint-disable react/prop-types */
 import React from "react";
-import './card.css'
+import "./card.css";
 
 export default function Card({cardElement, onFlip, flipIt, block}) { 
 
-  return (
-    <div className={`card ${flipIt ? 'see' : ''}`}  
-         onClick={() => !block ? onFlip(cardElement.id, cardElement) : ''}>
-    <div 
-        className="icon"> <img src={cardElement.src} ></img>  </div>
-    </div> 
-  )
+    return (
+        <div className={`card ${flipIt ? "see" : ""}`}  
+            onClick={() => !block ? onFlip(cardElement.id, cardElement) : ""}>
+            <div 
+                className="icon"> <img src={cardElement.src} ></img>  </div>
+        </div> 
+    );
 }
